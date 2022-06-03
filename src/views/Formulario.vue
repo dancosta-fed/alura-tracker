d3f5d774d4d7f585aa136860bd8747de244d2819
Daniel Costa <daniel@saudeagora.com.vc>
2022-06-03 16:26:13 -0300

    Made some progress with classes

---
+++ src/views/Formulario.vue
@@ -0,0 +1,73 @@
+<template>
+  <section> 
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjet"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { TipoDeNotificacao } from "@/interfaces/INotificacao";
+import { useStore } from "@/store";
+import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes";
+import {  defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Formulario",
+  props: {
+    id: {
+      type: String
+    }
+  },
+  mounted () {
+    if(this.id){
+      const projeto = this.store.state.projetos.find(proj => proj.id === this.id)
+      this.nomeDoProjeto = projeto?.nome || ''
+    }
+  },
+  data() {
+    return {
+      nomeDoProjeto: "",
+    };
+  },
+  methods: {
+    salvar() {
+      if (this.id){
+        this.store.commit(ALTERA_PROJETO, {
+          id: this.id,
+          nome: this.nomeDoProjeto
+        })
+      } else {
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+      }
+      this.nomeDoProjeto = ""
+      // this.notificar(TipoDeNotificacao.SUCESSO, 'Oba!!', 'O projeto foi cadastrado com sucesso!')
+      this.$router.push('/projetos')
+    },
+  },
+  notificar (tipo: TipoDeNotificacao, titulo: string, texto: string) {    
+    this.store.commit(NOTIFICAR, {
+      titulo,
+      texto,
+      tipo
+    })
+  },
+  setup () {
+    const store = useStore()
+    return {
+      store
+    }
+  }
+});
+</script>
